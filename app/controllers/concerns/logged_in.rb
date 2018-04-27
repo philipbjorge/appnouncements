8543fd67ea7c0c4a02b3e0275b204cbe2c5dd9c9
Philip Bjorge <philip@philipbjorge.com>
2018-04-27 16:57:33 +0700

    Remove unused code

---
+++ app/controllers/concerns/logged_in.rb
@@ -0,0 +1,11 @@
+module LoggedIn
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :logged_in_using_omniauth?
+  end
+
+  def logged_in_using_omniauth?
+    redirect_to '/' unless session[:userinfo].present?
+  end
+end
