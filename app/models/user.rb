a879cd796069248454386c104cd5b713d2af8ac0
Philip Bjorge <philip@philipbjorge.com>
2018-06-26 17:49:33 +0300

    Style devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,11 @@ class User < ApplicationRecord
   end
   
   devise :database_authenticatable, :registerable, :recoverable,
-                  :rememberable, :trackable, :validatable, :confirmable, :lockable,
+                  :rememberable, :trackable, :validatable, :confirmable, 
                   send_email_changed_notification: true, send_password_change_notification: true,
                   allow_unconfirmed_access_for: 7.days, reconfirmable: true,
                   reset_password_within: 1.hour, sign_in_after_reset_password: true,
-                  extend_remember_period: true,
-                  unlock_strategy: :email, lock_strategy: :failed_attempts, maximum_attempts: 10
+                  extend_remember_period: true
 
   has_many :apps, dependent: :destroy
   
