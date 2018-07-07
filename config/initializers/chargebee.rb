8ab427de21736055dfe8876ad5c87b1115d2567c
Philip Bjorge <philip@philipbjorge.com>
2018-07-07 19:19:21 +0200

    Fix chargebee on staging

--- config/initializers/chargebee.rb
+++ config/initializers/chargebee.rb
@@ -1,1 +1,2 @@
-ChargeBee.configure(Rails.application.credentials[Rails.env.to_sym][:chargebee])
+chargebee_env = ENV["CHARGEBEE_TEST_ENV"].present? ? :development : Rails.env.to_sym
+ChargeBee.configure(Rails.application.credentials[chargebee_env][:chargebee])
