86cc846fd3135cd75e22498b2343f8087d86fb01
Philip Bjorge <philip@philipbjorge.com>
2018-07-08 23:05:46 +0200

    Add start to config API

--- app/controllers/api/v1/release_notes_controller.rb
+++ app/controllers/api/v1/release_notes_controller.rb
@@ -1,19 +1,38 @@
 module Api::V1
   class ReleaseNotesController < ApplicationController
     skip_before_action :verify_authenticity_token
+    before_action :skip_authorization, only: [:show, :configuration]
 
     def show
-      skip_authorization
-
       @app = App.find_by(uuid: params[:uuid], disabled: false)
       not_found unless @app
 
       # TODO: Validate start/end_version
-      @releases = @app.releases.published
-      @releases = @releases.where("string_to_array(version, '.')::int[] >= string_to_array(?, '.')::int[]", params[:start_version]) if params[:start_version]
-      @releases = @releases.where("string_to_array(version, '.')::int[] <= string_to_array(?, '.')::int[]", params[:end_version]) if params[:end_version]
-
+      @releases = []
+      if params[:start_version] != params[:end_version]
+        @releases = @app.releases.published
+        @releases = @releases.where("string_to_array(version, '.')::int[] >= string_to_array(?, '.')::int[]", params[:start_version]) if params[:start_version]
+        @releases = @releases.where("string_to_array(version, '.')::int[] <= string_to_array(?, '.')::int[]", params[:end_version]) if params[:end_version]
+      end
+      
       render layout: "webview"
+    end
+    
+    def configuration
+      @app = App.find_by(uuid: params[:uuid], disabled: false)
+      not_found unless @app
+      
+      # TODO: Validate start/end_version
+      @releases = []
+      if params[:start_version] != params[:end_version]
+        @releases = @app.releases.published
+        @releases = @releases.where("string_to_array(version, '.')::int[] >= string_to_array(?, '.')::int[]", params[:start_version]) if params[:start_version]
+        @releases = @releases.where("string_to_array(version, '.')::int[] <= string_to_array(?, '.')::int[]", params[:end_version]) if params[:end_version]
+      end
+
+      render json: {
+          unseenReleasesCount: @releases.size
+      }
     end
 
     def preview
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'release_notes/:uuid(/:start_version...(:end_version))' => "release_notes#show"
       get 'release_notes/:uuid/...(:end_version)' => "release_notes#show", defaults: { start_version: nil }
+      
+      get 'configuration/:uuid(/:start_version...(:end_version))' => "release_notes#configuration"
+      get 'configuration/:uuid/...(:end_version)' => "release_notes#configuration", defaults: { start_version: nil }
 
       patch 'release_notes/:uuid/preview' => "release_notes#preview"
       post 'release_notes/:uuid/preview' => "release_notes#preview"
