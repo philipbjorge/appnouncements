286defcae8bcd97a807b7e8cfa7ddb73453a151f
Philip Bjorge <philip@philipbjorge.com>
2018-07-10 15:07:21 +0200

    Add mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,36 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+
+  if ENV['MAILTRAP_API_TOKEN'].present?
+    response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+  
+    first_inbox = JSON.parse(response)[0] # get first inbox
+  
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+        :user_name => first_inbox['username'],
+        :password => first_inbox['password'],
+        :address => first_inbox['domain'],
+        :domain => first_inbox['domain'],
+        :port => first_inbox['smtp_ports'][0],
+        :authentication => :plain
+    }
+  else
+    # Sengrid
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :domain => 'appnouncements.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
+  end
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
