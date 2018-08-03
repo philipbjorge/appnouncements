be09cc6672d38918a242289abbd931c3e4d0ba9b
Philip Bjorge <philip@philipbjorge.com>
2018-08-03 10:22:10 -0700

    Add staging chargebee

--- config/initializers/chargebee.rb
+++ config/initializers/chargebee.rb
@@ -1,6 +1,1 @@
-puts ENV["HEROKU_ENV"]
-puts ENV["RAILS_ENV"]
-puts Rails.env
-puts (ENV["HEROKU_ENV"] || ENV["RAILS_ENV"]).to_sym
-puts Rails.application.credentials
-ChargeBee.configure(Rails.application.credentials[(ENV["HEROKU_ENV"] || ENV["RAILS_ENV"]).to_sym][:chargebee])
+ChargeBee.configure(Rails.application.credentials[(ENV["HEROKU_ENV"] || ENV["RAILS_ENV"] || Rails.env).to_sym][:chargebee])
