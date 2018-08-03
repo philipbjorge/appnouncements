48d11494e4983a902b71cc99b321603fef758375
Philip Bjorge <philip@philipbjorge.com>
2018-08-03 10:14:27 -0700

    Debug

--- config/initializers/chargebee.rb
+++ config/initializers/chargebee.rb
@@ -1,1 +1,6 @@
+puts ENV["HEROKU_ENV"]
+puts ENV["RAILS_ENV"]
+puts Rails.env
+puts (ENV["HEROKU_ENV"] || ENV["RAILS_ENV"]).to_sym
+puts Rails.application.credentials
 ChargeBee.configure(Rails.application.credentials[(ENV["HEROKU_ENV"] || ENV["RAILS_ENV"]).to_sym][:chargebee])
