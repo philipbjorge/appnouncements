91710168bd778d1b677fbb40eae11a4bcf69f099
Philip Bjorge <philip@philipbjorge.com>
2018-08-14 22:10:58 -0700

    Make sentry async

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.current_environment = ENV["HEROKU_ENV"] || ENV["RAILS_ENV"]
+  config.async = lambda { |event| SentryJob.perform_later(event) }
 end
